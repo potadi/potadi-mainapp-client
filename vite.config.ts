edd49e5c46922b17f287914a9347278e1d30ab5b
eldivategar <tegareldiva@students.amikom.ac.id>
2024-10-12 22:28:04 +0700

    add vercel confoguration test

--- vite.config.ts
+++ vite.config.ts
@@ -29,42 +29,6 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: "/pwa/windows11/SmallTile.scale-100.png",
-            sizes: "71x71",
-          },
-          {
-            src: "/pwa/windows11/SmallTile.scale-125.png",
-            sizes: "89x89",
-          },
-          {
-            src: "/pwa/windows11/SmallTile.scale-150.png",
-            sizes: "107x107",
-          },
-          {
-            src: "/pwa/windows11/SmallTile.scale-200.png",
-            sizes: "142x142",
-          },
-          {
-            src: "/pwa/windows11/SmallTile.scale-400.png",
-            sizes: "284x284",
-          },
-          {
-            src: "/pwa/windows11/Square150x150Logo.scale-100.png",
-            sizes: "150x150",
-          },
-          {
-            src: "/pwa/windows11/Square150x150Logo.scale-125.png",
-            sizes: "188x188",
-          },
-          {
-            src: "/pwa/windows11/Square150x150Logo.scale-150.png",
-            sizes: "225x225",
-          },
-          {
-            src: "/pwa/windows11/Square150x150Logo.scale-200.png",
-            sizes: "300x300",
-          },
-          {
             src: "/pwa/windows11/Square150x150Logo.scale-400.png",
             sizes: "600x600",
           },
@@ -91,18 +55,6 @@ export default defineConfig({
           {
             src: "/pwa/windows11/LargeTile.scale-100.png",
             sizes: "310x310",
-          },
-          {
-            src: "/pwa/windows11/LargeTile.scale-125.png",
-            sizes: "388x388",
-          },
-          {
-            src: "/pwa/windows11/LargeTile.scale-150.png",
-            sizes: "465x465",
-          },
-          {
-            src: "/pwa/windows11/LargeTile.scale-200.png",
-            sizes: "620x620",
           },
           {
             src: "/pwa/windows11/LargeTile.scale-400.png",
@@ -169,176 +121,12 @@ export default defineConfig({
             sizes: "2480x1200",
           },
           {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-16.png",
-            sizes: "16x16",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-20.png",
-            sizes: "20x20",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-24.png",
-            sizes: "24x24",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-30.png",
-            sizes: "30x30",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-32.png",
-            sizes: "32x32",
-          },
-          {
             src: "/pwa/windows11/Square44x44Logo.targetsize-36.png",
             sizes: "36x36",
           },
           {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-40.png",
-            sizes: "40x40",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-44.png",
-            sizes: "44x44",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-48.png",
-            sizes: "48x48",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-60.png",
-            sizes: "60x60",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-64.png",
-            sizes: "64x64",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-72.png",
-            sizes: "72x72",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-80.png",
-            sizes: "80x80",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.targetsize-96.png",
-            sizes: "96x96",
-          },
-          {
             src: "/pwa/windows11/Square44x44Logo.targetsize-256.png",
             sizes: "256x256",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-16.png",
-            sizes: "16x16",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-20.png",
-            sizes: "20x20",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-24.png",
-            sizes: "24x24",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-30.png",
-            sizes: "30x30",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-32.png",
-            sizes: "32x32",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-36.png",
-            sizes: "36x36",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-40.png",
-            sizes: "40x40",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-44.png",
-            sizes: "44x44",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-48.png",
-            sizes: "48x48",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-60.png",
-            sizes: "60x60",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-64.png",
-            sizes: "64x64",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-72.png",
-            sizes: "72x72",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-80.png",
-            sizes: "80x80",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-96.png",
-            sizes: "96x96",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-unplated_targetsize-256.png",
-            sizes: "256x256",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-16.png",
-            sizes: "16x16",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-20.png",
-            sizes: "20x20",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-24.png",
-            sizes: "24x24",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-30.png",
-            sizes: "30x30",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-32.png",
-            sizes: "32x32",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-36.png",
-            sizes: "36x36",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-40.png",
-            sizes: "40x40",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-44.png",
-            sizes: "44x44",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-48.png",
-            sizes: "48x48",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-60.png",
-            sizes: "60x60",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-64.png",
-            sizes: "64x64",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-72.png",
-            sizes: "72x72",
-          },
-          {
-            src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-80.png",
-            sizes: "80x80",
           },
           {
             src: "/pwa/windows11/Square44x44Logo.altform-lightunplated_targetsize-96.png",
